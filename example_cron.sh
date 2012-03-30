70f7cd6f89a068e5b978710833bd94c28a52e4cb
Clint Sharp <clint@typhoon.org>
2012-03-29 19:15:59 -0600

    Fixing example_cron

--- example_cron.sh
+++ example_cron.sh
@@ -1,4 +1,5 @@
-$SPLUNKBOT_PATH=/path/to/splunkbot
-$NODE_BIN=/path/to/node
+#!/usr/bin/env sh
+SPLUNKBOT_PATH=/path/to/splunkbot
+NODE_BIN=/path/to/node
 cd $SPLUNKBOT_PATH
 $NODE_BIN $SPLUNKBOT_PATH/genmapjson.js
